7aa689fa08c55cee14072116886791262df7fe78
Abuzer337 <kukuruzafukin@gmail.com>
2025-06-09 04:03:30 +0300

    Update vite.config.js

--- devops-front/vite.config.js
+++ devops-front/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',  // Имя сервиса из docker-compose
+        changeOrigin: true,
+      }
+    }
+  }
 })
