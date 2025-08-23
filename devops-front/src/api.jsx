17f655e93761001fda7026f9972cf52d4906b181
ryman63 <portablehole63@gmail.com>
2025-08-23 23:24:47 +0300

    fix conn

--- devops-front/src/api.jsx
+++ devops-front/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://89.169.181.201:30080', // Укажите ваш URL
+  baseURL: 'http://89.169.181.201:30080',
 });
 
 // Контроллер машин
