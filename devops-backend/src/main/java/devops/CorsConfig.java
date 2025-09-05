1438d3f947aa3e7712c4a115a26b3fda5226d79c
ryman63 <portablehole63@gmail.com>
2025-09-05 13:41:43 +0300

    fix connection

--- devops-backend/src/main/java/devops/CorsConfig.java
+++ devops-backend/src/main/java/devops/CorsConfig.java
@@ -9,8 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //.allowedOrigins("http://158.160.168.152", "http://localhost:3000", "http://localhost", "http://localhost:8080")
-                .allowedOrigins("*")
+                .allowedOrigins("http://158.160.196.153", "http://localhost:3000", "http://localhost", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
