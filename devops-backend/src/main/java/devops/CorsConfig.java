c45cf7c27b3d47235c95ae31061c9f2139ea02cf
ryman63 <portablehole63@gmail.com>
2025-09-05 12:36:22 +0300

    fix connection

--- devops-backend/src/main/java/devops/CorsConfig.java
+++ devops-backend/src/main/java/devops/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost", "http://localhost:8080")
+                .allowedOrigins("http://frontend.example.com", "http://localhost:3000", "http://localhost", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- devops-front/src/api.jsx
+++ devops-front/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://frontend.example.com:8080',
 });
 
 // Контроллер машин
